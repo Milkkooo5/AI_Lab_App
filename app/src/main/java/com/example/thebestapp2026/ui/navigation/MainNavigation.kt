9e10eb51354372966de20e896a701e996652a0d3
Milana <ananbekh@icloud.com>
2026-05-23 11:58:56 +0300

    Исправлен BottomBar и добавление в навигацию скринов регистрации и авторизации

--- app/src/main/java/com/example/thebestapp2026/ui/navigation/MainNavigation.kt
+++ app/src/main/java/com/example/thebestapp2026/ui/navigation/MainNavigation.kt
@@ -1,6 +1,15 @@
 package com.example.thebestapp2026.ui.navigation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
@@ -10,9 +19,13 @@ import androidx.compose.material.icons.outlined.History
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.*
 import com.example.thebestapp2026.ui.screens.*
@@ -25,25 +38,33 @@ sealed class BottomItem(
     object Upload : BottomItem("upload", "Загрузка")
     object History : BottomItem("history", "История")
     object Profile : BottomItem("profile", "Профиль")
+    object Result : BottomItem("result", "Результат")
+
 }
 
 @Composable
 fun MainNavigation() {
 
     val navController = rememberNavController()
+    val currentRoute =
+        navController.currentBackStackEntryAsState().value?.destination?.route
 
     Scaffold(
+
         containerColor = Color(0xFFF8FAFF),
-
         bottomBar = {
-            BottomBar(navController)
+            if (
+                currentRoute != "login" &&
+                currentRoute != "register"
+            ) {
+                BottomBar(navController)
+            }
         }
-
     ) { padding ->
 
         NavHost(
             navController = navController,
-            startDestination = BottomItem.Home.route,
+            startDestination = "login",
             modifier = Modifier.padding(padding)
         ) {
 
@@ -56,106 +77,133 @@ fun MainNavigation() {
             }
 
             composable(BottomItem.History.route) {
-                HistoryScreen()
+                HistoryScreen( navController = navController)
             }
 
             composable(BottomItem.Profile.route) {
                 ProfileScreen()
             }
+            composable(BottomItem.Result.route) {
+                ResultScreen( navController = navController)
+            }
+            composable("login") {
+                LoginScreen(navController = navController)
+            }
+
+            composable("register") {
+                RegisterScreen(navController = navController)
+            }
+
         }
     }
 }
 
 @Composable
 fun BottomBar(
-    navController: NavHostController
+    navController: NavController,
+    modifier: Modifier = Modifier
 ) {
 
-    val items = listOf(
-        BottomItem.Home,
-        BottomItem.Upload,
-        BottomItem.History,
-        BottomItem.Profile
-    )
-
     Card(
-        modifier = Modifier.padding(
-            start = 18.dp,
-            end = 18.dp,
-            bottom = 18.dp
-        ),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(
+                horizontal = 20.dp,
+                vertical = 18.dp
+            ),
+
         shape = RoundedCornerShape(30.dp),
+
         colors = CardDefaults.cardColors(
             containerColor = Color.White
         ),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 10.dp
-        )
+
+        elevation = CardDefaults.cardElevation(10.dp)
     ) {
 
-        NavigationBar(
-            containerColor = Color.Transparent
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+
+            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
 
-            val navBackStackEntry by navController.currentBackStackEntryAsState()
-            val currentRoute = navBackStackEntry?.destination?.route
+            BottomBarItem(
+                title = "Главная",
+                isSelected = false
+            ) {
+                navController.navigate("home")
+            }
 
-            items.forEach { item ->
+            BottomBarItem(
+                title = "Загрузка",
+                isSelected = false
+            ) {
+                navController.navigate("upload")
+            }
 
-                NavigationBarItem(
-                    selected = currentRoute == item.route,
+            BottomBarItem(
+                title = "История",
+                isSelected = false
+            ) {
+                navController.navigate("history")
+            }
 
-                    onClick = {
-                        navController.navigate(item.route)
-                    },
-
-                    colors = NavigationBarItemDefaults.colors(
-                        selectedIconColor = Color(0xFF2F7DFF),
-                        selectedTextColor = Color(0xFF2F7DFF),
-                        indicatorColor = Color.Transparent,
-                        unselectedIconColor = Color.LightGray,
-                        unselectedTextColor = Color.LightGray
-                    ),
-
-                    icon = {
-
-                        when (item.route) {
-
-                            "home" -> {
-                                Icon(
-                                    imageVector = Icons.Default.Home,
-                                    contentDescription = null
-                                )
-                            }
-
-                            "upload" -> {
-                                Icon(
-                                    imageVector = Icons.Default.Upload,
-                                    contentDescription = null
-                                )
-                            }
-
-                            "history" -> {
-                                Icon(
-                                    imageVector = Icons.Outlined.History,
-                                    contentDescription = null
-                                )
-                            }
-
-                            else -> {
-                                Icon(
-                                    imageVector = Icons.Default.Person,
-                                    contentDescription = null
-                                )
-                            }
-                        }
-                    },
-
-                    label = {
-                        Text(text = item.title)
-                    }
-                )
+            BottomBarItem(
+                title = "Профиль",
+                isSelected = false
+            ) {
+                navController.navigate("profile")
             }
         }
+    }
+}
+
+@Composable
+fun BottomBarItem(
+    title: String,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        modifier = Modifier.clickable {
+            onClick()
+        }
+    ) {
+
+        Box(
+            modifier = Modifier
+                .size(14.dp)
+                .background(
+                    if (isSelected)
+                        Color(0xFF2F7DFF)
+                    else
+                        Color(0xFFE5E7EB),
+
+                    CircleShape
+                )
+        )
+
+        Text(
+            text = title,
+
+            fontSize = 11.sp,
+
+            fontWeight = if (isSelected)
+                FontWeight.Bold
+            else
+                FontWeight.Normal,
+
+            color = if (isSelected)
+                Color(0xFF2F7DFF)
+            else
+                Color(0xFF6B7280),
+
+            modifier = Modifier.padding(top = 6.dp)
+        )
     }
 }
