a0103053bc47477554272edcde9cd6832a4cb60f
Milana <ananbekh@icloud.com>
2026-05-25 22:20:23 +0300

    Добавлен retrofit клиент для подключения к серверу

--- app/src/main/java/com/example/thebestapp2026/data/remote/KtorClient.kt
+++ app/src/main/java/com/example/thebestapp2026/data/remote/KtorClient.kt
@@ -8,20 +8,18 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 
 import kotlinx.serialization.json.Json
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-object KtorClient {
+object RetrofitClient {
 
-    const val BASE_URL = "http://10.0.2.2:8080"
+    private const val BASE_URL = "http://192.168.78.21:8080/"
 
-    val client = HttpClient(Android) {
-
-        install(ContentNegotiation) {
-
-            json(
-                Json {
-                    ignoreUnknownKeys = true
-                }
-            )
-        }
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
     }
 }
