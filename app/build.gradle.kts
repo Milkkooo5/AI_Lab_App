8f99218e053b958c69e3be947077ef15fdd12075
Milana <ananbekh@icloud.com>
2026-05-25 21:58:23 +0300

    Добавлены зависимости для retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,4 +90,10 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.5")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 }
