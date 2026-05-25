46592a0027f5f0e988ddb953bf2ff0e2cafee588
Milana <ananbekh@icloud.com>
2026-05-25 23:02:48 +0300

    Изменения в api сервисе и auth dto

--- app/src/main/java/com/example/thebestapp2026/data/remote/ApiService.kt
+++ app/src/main/java/com/example/thebestapp2026/data/remote/ApiService.kt
@@ -5,9 +5,12 @@ import android.net.Uri
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
+import retrofit2.http.Path
 
 interface ApiService {
 
@@ -24,7 +27,15 @@ interface ApiService {
     @Multipart
     @POST("analysis/upload")
     suspend fun uploadAnalysis(
+        @Header("Authorization")
+        token: String,
         @Part("userId") userId: RequestBody,
         @Part file: MultipartBody.Part
     ): AnalyzeResponse
+
+    @GET("analysis/history/{userId}")
+    suspend fun getHistory(
+        @Header("Authorization") token: String,
+        @Path("userId") userId: String
+    ): List<AnalyzeResponse>
 }
--- app/src/main/java/com/example/thebestapp2026/data/remote/AuthDTO.kt
+++ app/src/main/java/com/example/thebestapp2026/data/remote/AuthDTO.kt
@@ -16,19 +16,18 @@ data class RegisterRequest(
 
 @Serializable
 data class LoginRequest(
-
     val email: String,
     val password: String
 )
 
 @Serializable
 data class AuthResponse(
-
     val userId: String,
     val name: String,
     val surname: String,
     val email: String,
     val birthDate: String,
     val city: String,
-    val gender: String
+    val gender: String,
+    val token: String
 )
