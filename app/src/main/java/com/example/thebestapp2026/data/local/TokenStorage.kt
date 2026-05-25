8135f4d26e6775be826aed7780f141d215b0f96a
Milana <ananbekh@icloud.com>
2026-05-25 23:00:03 +0300

    Добавлено хранение токена на android

---
+++ app/src/main/java/com/example/thebestapp2026/data/local/TokenStorage.kt
@@ -0,0 +1,20 @@
+package com.example.thebestapp2026.data.local
+
+
+
+import android.content.Context
+class TokenStorage(context: Context) {
+    private val prefs = context.getSharedPreferences("auth", Context.MODE_PRIVATE)
+
+    fun saveToken(token: String) {
+        prefs.edit().putString("token", token).apply()
+    }
+
+    fun getToken(): String {
+        return prefs.getString("token", "") ?: ""
+    }
+
+    fun clear() {
+        prefs.edit().clear().apply()
+    }
+}
