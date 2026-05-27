22a7460825fd0435c807581be09c187ae7b4a161
Milana <ananbekh@icloud.com>
2026-05-28 02:44:40 +0300

    В KtorClient улучшена обработка timeout ошибок

--- app/src/main/java/com/example/thebestapp2026/data/remote/KtorClient.kt
+++ app/src/main/java/com/example/thebestapp2026/data/remote/KtorClient.kt
@@ -8,16 +8,25 @@ import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 
 import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
     private const val BASE_URL = "http://192.168.78.21:8080/"
 
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
+        .build()
+
     val api: ApiService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
