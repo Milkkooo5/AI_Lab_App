c361ab933a37f90342a934122c963a3fec8632e8
Milana <ananbekh@icloud.com>
2026-06-08 22:33:05 +0300

    Изменения в KtorClient ( замена ip адреса физического телефона на адрес эмулятора)

--- app/src/main/java/com/example/thebestapp2026/data/remote/KtorClient.kt
+++ app/src/main/java/com/example/thebestapp2026/data/remote/KtorClient.kt
@@ -14,9 +14,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-
     private const val BASE_URL = "http://192.168.78.21:8080/"
-
     private val okHttpClient = OkHttpClient.Builder()
         .connectTimeout(60, TimeUnit.SECONDS)
         .readTimeout(60, TimeUnit.SECONDS)
